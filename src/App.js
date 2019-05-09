e02faa1c55ae22c787698fae21342c9fcb4c2626
Shurui Liu <shurui91@gmail.com>
2019-05-09 11:34:29 -0700

    working on footer part

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import Navigation from './components/Navigation';
+import Footer from './components/Footer';
 
 function App() {
 	return (
 		<div>
 			<Navigation />
+			<div id="content" />
+			<Footer />
 		</div>
 	);
 }
