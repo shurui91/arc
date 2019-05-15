d2c6bf598fddd11c1b4d73acc8a35a7631034d73
Shurui Liu <shurui91@gmail.com>
2019-05-15 11:10:27 -0700

    working on support page

---
+++ src/components/Support.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './Support.css';
+
+class Support extends Component {
+	render() {
+		return <support className="Support">test</support>;
+	}
+}
+
+export default Support;
