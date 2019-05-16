a63a1cc76fe35dc6f67c4a5bef812bcd98141dee
Shurui Liu <shurui91@gmail.com>
2019-05-16 14:59:23 -0700

    navbar is done without any styles

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
 import './Navigation.css';
 
 class Navigation extends Component {
 	render() {
-		return <div>Navigation;</div>;
+		return (
+			<div>
+				<NavLink to="/">Home</NavLink>
+				<NavLink to="/vision">Vision</NavLink>
+				<NavLink to="/about">About Us</NavLink>
+				<NavLink to="/project">Featured Projects</NavLink>
+				<NavLink to="/publication">Publications</NavLink>
+				<NavLink to="/support">Support ARC</NavLink>
+			</div>
+		);
 	}
 }
 
