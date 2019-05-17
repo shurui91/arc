038700dc1706bd8b4458188553acc636b3f64c31
Shurui Liu <shurui91@gmail.com>
2019-05-16 18:07:49 -0700

    aboutus page is done

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
 import './About.css';
+import { ListGroup } from 'react-bootstrap';
 
 class About extends Component {
 	render() {
 		return (
-			<div>
-				<p>About</p>
+			<div className="about">
+				<h2>The goal of ARC is to provide a platform by setting up an institute to</h2>
+				<ListGroup>
+					<ListGroup.Item>
+						Bridge the gap between the East and West in targeted areas such as culture, language, medicine
+						and religion
+					</ListGroup.Item>
+					<ListGroup.Item>
+						Establish an eminent research and education center to train scholars and produce world class and
+						impactful research
+					</ListGroup.Item>
+					<ListGroup.Item>
+						Promote an increased awareness and knowledge between the East and West
+					</ListGroup.Item>
+					<ListGroup.Item>
+						Remove/minimize misunderstandings and biases that are currently upheld by the general public
+					</ListGroup.Item>
+					<ListGroup.Item>
+						Propagate/disseminate truths to society in general and scholars in particular
+					</ListGroup.Item>
+					<ListGroup.Item>
+						Teach relevant topics in the Asian context to businessmen and scholars
+					</ListGroup.Item>
+				</ListGroup>
 			</div>
 		);
 	}
