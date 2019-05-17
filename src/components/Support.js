6ef2259b040a714c7ab3c59d6e0de238b09b05cd
Shurui Liu <shurui91@gmail.com>
2019-05-16 18:28:33 -0700

    support page is done

--- src/components/Support.js
+++ src/components/Support.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import './Support.css';
+import { Jumbotron } from 'react-bootstrap';
 
 class Support extends Component {
 	render() {
 		return (
-			<div>
-				<p>Support</p>
+			<div className="support">
+				<Jumbotron>
+					<h1>Support ARC</h1>
+					<p>
+						To support ARC please send your donation via money order or check to:
+						<br />
+						Asia Research Center 27611 La Paz Road Laguna Niguel, CA 92677
+						<br />
+						For more information please contact us at 714-726-7088
+					</p>
+				</Jumbotron>
 			</div>
 		);
 	}
