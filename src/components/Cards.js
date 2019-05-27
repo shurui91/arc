0ba70f0dd4d3865b9465fd645d403ace9e875c5f
Shurui Liu <shurui91@gmail.com>
2019-05-27 13:37:04 -0700

    card part updated, font size bigger, margin changed, align center

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,53 +1,55 @@
 import React, { Component } from 'react';
 // import { Timeline, TimelineItem } from 'vertical-timeline-component-for-react';
 import './Cards.css';
-import { Card, Row, Col } from 'react-bootstrap';
+import { Card, Container, Row, Col } from 'react-bootstrap';
 
 class Cards extends Component {
 	render() {
 		return (
 			<div className="cards">
-				<Row>
-					<Col xs={12} md={4}>
-						<Card style={{ width: '24rem' }}>
-							<Card.Img variant="top" src={require('../Participate.jpg')} />
-							<Card.Body>
-								<Card.Title>Contact Us</Card.Title>
-								<Card.Text>
-									Some quick example text to build on the card title and make up the bulk of the
-									card's content.
+				<Container>
+					<Row>
+						<Col xs={12} md={4}>
+							<Card style={{ width: '22rem' }}>
+								<Card.Img variant="top" src={require('../Participate.jpg')} />
+								<Card.Body>
+									<Card.Title>Contact Us</Card.Title>
+									<Card.Text>
+										Some quick example text to build on the card title and make up the bulk of the
+										card's content.
 								</Card.Text>
-								{/* <Button variant="primary">Go somewhere</Button> */}
-							</Card.Body>
-						</Card>
-					</Col>
-					<Col xs={12} md={4}>
-						<Card style={{ width: '24rem' }}>
-							<Card.Img variant="top" src={require('../Participate.jpg')} />
-							<Card.Body>
-								<Card.Title>About Us</Card.Title>
-								<Card.Text>
-									Some quick example text to build on the card title and make up the bulk of the
-									card's content.
+									{/* <Button variant="primary">Go somewhere</Button> */}
+								</Card.Body>
+							</Card>
+						</Col>
+						<Col xs={12} md={4}>
+							<Card style={{ width: '22rem' }}>
+								<Card.Img variant="top" src={require('../Participate.jpg')} />
+								<Card.Body>
+									<Card.Title>About Us</Card.Title>
+									<Card.Text>
+										Some quick example text to build on the card title and make up the bulk of the
+										card's content.
 								</Card.Text>
-								{/* <Button variant="primary">Go somewhere</Button> */}
-							</Card.Body>
-						</Card>
-					</Col>
-					<Col xs={12} md={4}>
-						<Card style={{ width: '24rem' }}>
-							<Card.Img variant="top" src={require('../Participate.jpg')} />
-							<Card.Body>
-								<Card.Title>Who We Are</Card.Title>
-								<Card.Text>
-									Some quick example text to build on the card title and make up the bulk of the
-									card's content.
+									{/* <Button variant="primary">Go somewhere</Button> */}
+								</Card.Body>
+							</Card>
+						</Col>
+						<Col xs={12} md={4}>
+							<Card style={{ width: '22rem' }}>
+								<Card.Img variant="top" src={require('../Participate.jpg')} />
+								<Card.Body>
+									<Card.Title>Who We Are</Card.Title>
+									<Card.Text>
+										Some quick example text to build on the card title and make up the bulk of the
+										card's content.
 								</Card.Text>
-								{/* <Button variant="primary">Go somewhere</Button> */}
-							</Card.Body>
-						</Card>
-					</Col>
-				</Row>
+									{/* <Button variant="primary">Go somewhere</Button> */}
+								</Card.Body>
+							</Card>
+						</Col>
+					</Row>
+				</Container>
 			</div>
 		);
 	}
