db11e60a3a93bc9de6ec47e04a208a2afb9d9356
Shurui Liu <shurui91@gmail.com>
2019-05-27 20:44:15 -0700

    still working on partner page

--- src/components/PartnerImages.js
+++ src/components/PartnerImages.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react';
 import './PartnerImages.css';
+import { Container, Row, Col } from 'react-bootstrap';
 
 class PartnerImages extends Component {
-	render() {
-		return (
-			<div className="partnersImage">
-				<div className="content">
-                    <p>Our Partner - Orange County Music and Dance</p>
-                </div>
-			</div>
-		);
-	}
+    render() {
+        return (
+            <div className="partnersImage">
+                <Row>
+                    <Col sm={12}>
+                        <img src={require('../scholarship.jpg')}
+                        style={{ width: '100%' }} />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col xl={6}>
+                        <img src={require('../enrichment.jpg')}
+                         />
+                    </Col>
+                    <Col xl={6}>
+                        <img src={require('../happy-violin-girls.jpg')}
+                         />
+                    </Col>
+                </Row>
+            </div>
+        );
+    }
 }
 
 export default PartnerImages;
