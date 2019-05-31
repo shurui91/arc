a8f18cae9de693758b7075c5f9e76b3f83d98d25
Shurui Liu <shurui91@gmail.com>
2019-05-30 22:57:27 -0700

    partner part image updated

--- src/components/PartnerImages.js
+++ src/components/PartnerImages.js
@@ -3,28 +3,17 @@ import './PartnerImages.css';
 import { Container, Row, Col } from 'react-bootstrap';
 
 class PartnerImages extends Component {
-    render() {
-        return (
-            <div className="partnersImage">
-                <Row>
-                    <Col sm={12}>
-                        <img src={require('../scholarship.jpg')}
-                        style={{ width: '100%' }} />
-                    </Col>
-                </Row>
-                <Row>
-                    <Col xl={6}>
-                        <img src={require('../enrichment.jpg')}
-                         />
-                    </Col>
-                    <Col xl={6}>
-                        <img src={require('../happy-violin-girls.jpg')}
-                         />
-                    </Col>
-                </Row>
-            </div>
-        );
-    }
+	render() {
+		return (
+			<div className="partnersImage">
+				<Row>
+					<Col sm={12}>
+						<img style={{ width: '100%' }} src={require('../20190529214043.jpg')} alt="" />
+					</Col>
+				</Row>
+			</div>
+		);
+	}
 }
 
 export default PartnerImages;
