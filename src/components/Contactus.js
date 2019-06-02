4cfa3c33756cde3eff138d1503789ccc12a416c9
Shurui Liu <shurui91@gmail.com>
2019-06-01 23:09:39 -0700

    contactus page updated

--- src/components/Contactus.js
+++ src/components/Contactus.js
@@ -1,11 +1,43 @@
 import React, { Component } from 'react';
 import './Contactus.css';
 
+const styles = {
+	fontFamily: "sans-serif",
+	textAlign: "center"
+};
+
 class Contactus extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { value: '' };
+
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	handleChange(event) {
+		this.setState({ value: event.target.value });
+	}
+
+	handleSubmit(event) {
+		alert('A name was submitted: ' + this.state.value);
+		event.preventDefault();
+	}
+
 	render() {
 		return (
-			<div>
-				<p>test</p>
+			<div className="contactForm">
+				<form onSubmit={this.handleSubmit}>
+					<div>
+						<label>Name </label>
+						<input type="text" value={this.state.value} onChange={this.handleChange} />
+					</div>
+					<div>
+						<label>Email </label>
+						<input type="text" value={this.state.value} onChange={this.handleChange} />
+					</div>
+					<input type="submit" value="Submit" />
+				</form>
 			</div>
 		);
 	}
