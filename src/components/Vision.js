5306541868202328e2b3797f7ad056b886ee1809
Shurui Liu <shurui91@gmail.com>
2019-06-04 09:27:31 -0700

    vision part updated, added padding

--- src/components/Vision.js
+++ src/components/Vision.js
@@ -7,8 +7,11 @@ class Vision extends Component {
 			<div className="vision">
 				<div className="visionContent">
 					<p style={{ color: '#7d9faa', fontSize: '38px' }}>Vision</p>
-					<p style={{ color: 'white', fontSize: '21px', textAlign: 'justify' }}>"A center to promote a better understanding of Asia’s rich cultures, languages and religions and how they integrate into a diverse global environment.​" 
-​Information—to Enrich​-​Interaction—to Exchange​-​Instruction—to Equip</p>
+					<p style={{ color: 'white', fontSize: '21px', textAlign: 'justify' }}>
+						"A center to promote a better understanding of Asia’s rich cultures, languages and religions and
+						how they integrate into a diverse global environment.​" ​Information—to Enrich​-​Interaction—to
+						Exchange​-​Instruction—to Equip
+					</p>
 				</div>
 			</div>
 		);
