b55d14e9db204ef2b043077d549ab53729a0d832
Shurui Liu <shurui91@gmail.com>
2019-06-04 09:10:07 -0700

    about us page updated

--- src/components/News.js
+++ src/components/News.js
@@ -5,112 +5,186 @@ import { Media } from 'react-bootstrap';
 class News extends Component {
 	render() {
 		return (
-			<div className="news">
-				<h3>Philanthropic</h3>
-				<Media>
-					<img
-						width={64}
-						height={64}
-						className="mr-3"
-						src={require('../neco.jpg')}
-						alt="Generic placeholder"
-					/>
-					<Media.Body>
-						<h5>Ellis Island Medal of Honor</h5>
-						<p>ARC Founder Charlie Zhang was the recent receipient of coveted Ellis</p>
-						<p>
-							The award pays homage to major contributions by American immigrants to the United States.
-							The Ellis Island Medals of Honor, and its recipients are read into the Congressional Record.
-						</p>
-						<p>
-							Past medalists include six Presidents, as well as Nobel Prize winners and leaders of
-							industry, education, the arts, sports and government. ARC and its members are dedicated to
-							the spreading of knowledge, education, charitible contributions, benevolent public policy
-							and economic growth.
-						</p>
-					</Media.Body>
-				</Media>
-				<hr sm="12" />
-				<Media>
-					<Media.Body>
-						<h5>ARC Roles</h5>
-						<p>
-							ARC directives are to compile, syntheize and analyze international issues​ ​in areas of
-							commerce, religion, government, policy and philapthropic​ ​areas. The coverage and awareness
-							of these issues can help to provide a​ ​clearer picture as well as insight for stakeholders
-							both in Asia and the​ ​United States. ARC believes Multilateral cooperation and good will
-							comes​ ​from research and relationship based mutual understanding.
-						</p>
-					</Media.Body>
-				</Media>
-				<hr sm="12" />
-				<Media>
-					<img
-						width={64}
-						height={64}
-						className="mr-3"
-						src={require('../fudan.jpg')}
-						alt="Generic placeholder"
-					/>
-					<Media.Body>
-						<h5>Asia Youth And Education</h5>
-						<p>
-							ARC is very active in their financial sponsorship and advisory support to institutions such
-							as the New Century International College in Jiangsu. The goal of ARCs involvement in this
-							and other youth and educational insitutions, is to widen the cultural and educational bridge
-							between Asia and the United States.
-						</p>
-					</Media.Body>
-				</Media>
-				<hr sm="12" />
-				<Media>
-					<img
-						width={64}
-						height={64}
-						className="mr-3"
-						src={require('../pacific.jpg')}
-						alt="Generic placeholder"
-					/>
-					<Media.Body>
-						<h5>Pacific Symphony</h5>
-						<p>
-							"Charlie Zhang has been on the Board of Directors for the Pacific Symphony of Orange County,
-							his involvement includes financial contributions, working with the students and countless
-							hours of mentorship. Most recently Charlie will be hosting a trip for 100 of the youth
-							orchestra students on a trip to Asia to gain a further understanding of the cultural, social
-							and religious environment in Asia, to foster music appreciation and greater cultural
-							understanding for the next generation of United State and Chinese leaders.
-						</p>
-					</Media.Body>
-				</Media>
-				<hr sm="12" />
-				<Media>
-					<img
-						width={64}
-						height={64}
-						className="mr-3"
-						src={require('../tedx.jpg')}
-						alt="Generic placeholder"
-					/>
-					<Media.Body>
-						<h5>TED X</h5>
-						<p>
-							ARC founder Charles Zhang was recently featured as a speaker in the internationally renoun
-							TEDX, his speech was given at La Sierra University and is spreading throughout the internet.
-							This heartfelt presentation is directed toward anyone seeking to grow and advance in what is
-							often challenging conditions. "The Power of One Moment" -- this message proves that anything
-							is possible when one can take the best of both Eastern and Western cultures.
-						</p>
-						<p>
-							His story is an inspiration to many Asian youth who will follow in his footsteps and gives
-							students and citizens alike a chance to see what it takes to make it in this country.
-							Getting a clearer picture of what citizens in the United States and abroad want is important
-							for policymakers, because public attitudes will shape prospects for effective multilateral
-							cooperation in the twenty-first century.
-						</p>
-					</Media.Body>
-				</Media>
-				<hr sm="12" />
+			<div>
+				<div className="news">
+					<h3>Philanthropic</h3>
+					<Media>
+						<img
+							width={64}
+							height={64}
+							className="mr-3"
+							src={require('../neco.jpg')}
+							alt="Generic placeholder"
+						/>
+						<Media.Body>
+							<h5>Ellis Island Medal of Honor</h5>
+							<p>ARC Founder Charlie Zhang was the recent receipient of coveted Ellis</p>
+							<p>
+								The award pays homage to major contributions by American immigrants to the United
+								States. The Ellis Island Medals of Honor, and its recipients are read into the
+								Congressional Record.
+							</p>
+							<p>
+								Past medalists include six Presidents, as well as Nobel Prize winners and leaders of
+								industry, education, the arts, sports and government. ARC and its members are dedicated
+								to the spreading of knowledge, education, charitible contributions, benevolent public
+								policy and economic growth.
+							</p>
+						</Media.Body>
+					</Media>
+					<hr sm="12" />
+					<Media>
+						<Media.Body>
+							<h5>ARC Roles</h5>
+							<p>
+								ARC directives are to compile, syntheize and analyze international issues​ ​in areas of
+								commerce, religion, government, policy and philapthropic​ ​areas. The coverage and
+								awareness of these issues can help to provide a​ ​clearer picture as well as insight for
+								stakeholders both in Asia and the​ ​United States. ARC believes Multilateral cooperation
+								and good will comes​ ​from research and relationship based mutual understanding.
+							</p>
+						</Media.Body>
+					</Media>
+					<hr sm="12" />
+					<Media>
+						<img
+							width={64}
+							height={64}
+							className="mr-3"
+							src={require('../fudan.jpg')}
+							alt="Generic placeholder"
+						/>
+						<Media.Body>
+							<h5>Asia Youth And Education</h5>
+							<p>
+								ARC is very active in their financial sponsorship and advisory support to institutions
+								such as the New Century International College in Jiangsu. The goal of ARCs involvement
+								in this and other youth and educational insitutions, is to widen the cultural and
+								educational bridge between Asia and the United States.
+							</p>
+						</Media.Body>
+					</Media>
+					<hr sm="12" />
+					<Media>
+						<img
+							width={64}
+							height={64}
+							className="mr-3"
+							src={require('../pacific.jpg')}
+							alt="Generic placeholder"
+						/>
+						<Media.Body>
+							<h5>Pacific Symphony</h5>
+							<p>
+								"Charlie Zhang has been on the Board of Directors for the Pacific Symphony of Orange
+								County, his involvement includes financial contributions, working with the students and
+								countless hours of mentorship. Most recently Charlie will be hosting a trip for 100 of
+								the youth orchestra students on a trip to Asia to gain a further understanding of the
+								cultural, social and religious environment in Asia, to foster music appreciation and
+								greater cultural understanding for the next generation of United State and Chinese
+								leaders.
+							</p>
+						</Media.Body>
+					</Media>
+					<hr sm="12" />
+					<Media>
+						<img
+							width={64}
+							height={64}
+							className="mr-3"
+							src={require('../tedx.jpg')}
+							alt="Generic placeholder"
+						/>
+						<Media.Body>
+							<h5>TED X</h5>
+							<p>
+								ARC founder Charles Zhang was recently featured as a speaker in the internationally
+								renoun TEDX, his speech was given at La Sierra University and is spreading throughout
+								the internet. This heartfelt presentation is directed toward anyone seeking to grow and
+								advance in what is often challenging conditions. "The Power of One Moment" -- this
+								message proves that anything is possible when one can take the best of both Eastern and
+								Western cultures.
+							</p>
+							<p>
+								His story is an inspiration to many Asian youth who will follow in his footsteps and
+								gives students and citizens alike a chance to see what it takes to make it in this
+								country. Getting a clearer picture of what citizens in the United States and abroad want
+								is important for policymakers, because public attitudes will shape prospects for
+								effective multilateral cooperation in the twenty-first century.
+							</p>
+						</Media.Body>
+					</Media>
+					<hr sm="12" />
+					<Media>
+						<Media.Body>
+							<h5>Charles Zhang, Treasurer</h5>
+							<p>
+								949.201.8270
+								<br />
+								charleszhang58@yahoo.com
+							</p>
+							<p>
+								Charles Zhang, founder of Pick Up Stix, embodies the American Dream – he started with
+								nothing but founded the leading Asian fast-casual dining restaurant chain in the
+								country. <br />
+								<br /> Charles Zhang immigrated to the United States in 1980 from Shanghai, China at the
+								age of 25. He did not speak English and had only US $20.00. Within four years, after
+								working as a liquor store clerk, busboy, waiter and gas stations, he had learned to
+								speak English and saved enough money to launch his first restaurant, Shanghai Charlie’s,
+								in Capistrano Beach, California. In 1989 he opened his second full-service restaurant,
+								Stix, in Laguna Niguel, Calif.
+								<br />
+								<br /> With the success of the Stix restaurant, it became evident that he was a savvy
+								businessman and an up-and-comer in the restaurant industry. The first Pick Up Stix
+								restaurant opened in 1990 in Rancho Santa Margarita, Calif. <br />
+								<br />
+								Within a decade, he had built an empire with more than 100 Pick Up Stix locations, a
+								warehouse and Distribution Company, and a commissary named Cal International Foods, Inc.
+								that streamlined the preparation of ingredients ensuring that each restaurant serves the
+								freshest products available with quality and consistency. <br />
+								<br />
+								Recognizing the potential for enormous growth by being part of a large well-respected
+								restaurant organization, he sold his restaurant businesses and the distribution center
+								to TGIF on June 30, 2001. <br />
+								<br />
+								Among his many achievements, he was named “Entrepreneur of the Year” in 1997 by
+								accounting firm of Ernst and Young and received the “Spirit of Life” award in 2002 from
+								the City of Hope. In addition, he received “Distinguished Business for Science and
+								Technology” award in May 2008, and “Beijing International Entrepreneur of the year in
+								October, 2008”. <br />
+								<br />
+								He retired from Pick Up Stix as the President and CEO on 5/31/2003; however, he
+								continuous to serve as a consultant and advisor for PUS. He has divested all his
+								interests in PUS to expand his real estate development company, Zion Enterprises, LLC.
+								He and his team members have successfully acquired a few pieces of land and redeveloped
+								them into medical buildings, office buildings, storages, residential, retail shopping
+								centers, and RV Golf Park. All these projects turned out to be the best investment
+								return. <br />
+								<br />
+								In addition, he is the CEO & President of Aseptic Solutions USA , which is a state-of
+								the art aseptic beverage bottle designed to manufacture a better tasting and longer
+								lasting product with extended shelf life that is capable of keeping food safe, fresh and
+								flavorful for over a year without refrigeration or preservatives. The company’s anchor
+								customers are Bolthouse, Naked Juice, Sambazon, and others... <br />
+								<br />
+								He resides in Laguna Niguel with his wife and two sons. Both he and his wife are very
+								active in their church life. He has been devoting his time helping his churches to
+								develop sanctuary meeting halls in San Juan Capistrano, Irvine, Cerritos, and New
+								Jersey.
+							</p>
+						</Media.Body>
+					</Media>
+					<hr sm="12" />
+					<Media>
+						<Media.Body>
+							<h5>Abraham Ho, Vice-President</h5>
+							<p>
+								888-5011-ARC (272) ext. 102 <br />
+								<br />
+							</p>
+						</Media.Body>
+					</Media>
+				</div>
 			</div>
 		);
 	}
