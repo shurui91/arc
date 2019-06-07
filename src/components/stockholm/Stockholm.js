0218e1479c03ea73eac0e06936e865d5c898f9af
Shurui Liu <shurui91@gmail.com>
2019-06-07 01:22:47 -0700

    added a slideshow sample

--- src/components/stockholm/Stockholm.js
+++ src/components/stockholm/Stockholm.js
@@ -1,20 +1,32 @@
 import React, { Component } from 'react';
 import './Stockholm.css';
-import { Button } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Container, Row, Col } from 'react-bootstrap';
+import 'react-bootstrap-carousel/dist/react-bootstrap-carousel.css';
+import Carousel from '../Carousel';
+import Footer from '../Footer';
 
 class Stockholm extends Component {
 	render() {
 		return (
 			<div>
 				<div className="stockholm">
-					<h1>EVENTS</h1>
-					<Link to="/books">
-						<Button type="button" className="float-right">
-							Read
-						</Button>
-					</Link>
+					<h1>Sinicization of Christianity in China</h1>
+					<Container>
+						<Row>
+							<Col xs={12} sm={12} md={12} lg={12} xl={6}>
+								<Carousel />
+							</Col>
+							<Col xs={12} sm={12} md={12} lg={12} xl={6}>
+								<h3>Sinicization of Christianity in China</h3>
+								<p>
+									Organized by the Fudan-European Centre for China Studies, UCPH and the Asia Research
+									Center and Center on Globalization and Religious Studies, Fudan University
+								</p>
+							</Col>
+						</Row>
+					</Container>
 				</div>
+				<Footer />
 			</div>
 		);
 	}
