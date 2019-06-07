2bab38f8a592c631b020c7fec949ee11ac1ea0b2
AaronL <shurui91@gmail.com>
2019-06-06 23:47:46 -0700

    books page updated again, width changed

--- src/components/Books.js
+++ src/components/Books.js
@@ -20,22 +20,25 @@ class Books extends Component {
 					<Table style={{ tableLayout: 'fixed', width: '100%' }}>
 						<tbody>
 							<tr>
-								<td class="image">
-									<img src={require('../20190601231935.png')} style={{ width: '70%' }} alt="" />
+								<td className="image">
+									<img src={require('../20190601231935.png')} style={{ width: '90%' }} alt="" />
 								</td>
-								<td class="intro">
+								<td className="intro">
 									<h3>和合本圣经百周年纪念</h3>
 									<p align="justify">
 										和合本圣经自1919年出版发行以来，从文学和信仰等方面，对中国社会作了极大的贡献。在文学方面，因着1919年发生了“五四运动”，一个新文学的时代正式开始。因此，和合本圣经可说是“生逢其时”，成为白话文的标准典范，展现出作为文学作品极高的价值。从这一方面而言，和合本圣经对于中国新文学的贡献，可与马丁·路德以德文翻译圣经对德国文学之贡献相提并论。
+									</p>
+									<p align="justify">
+										在信仰方面，基督信仰自从唐朝传入中国，数百年内一直“水土不服”，不能融入中国社会。这与和合本圣经之前的各译本采用文言文或方言翻译有直接的关系。和合本圣经的翻译原则之一就是“字词应当是操官话的平民百姓所日常使用和明白的，书面用语和不大通用的用词应该避免；文句的结构应符合口语”。因此，二十世纪以来，和合本圣经一旦面世，销量很大，流传很广，基督信仰也就随着和合本圣经而入千家万户，影响社会各阶层，并且深入人心。圣经体现的价值观如“荣神益人”、“爱人如己”、“和平福音”、“披戴基督”等，对净化社会道德空气、维持邻里和睦、社会和谐，居功甚伟。
 									</p>
 									<p>{/*<a href={kicus}>Read</a>*/}</p>
 								</td>
 							</tr>
 							<tr>
-								<td class="image">
-									<img src={require('../20190505225128.png')} style={{ width: '70%' }} alt="" />
+								<td className="image">
+									<img src={require('../20190505225128.png')} style={{ width: '90%' }} alt="" />
 								</td>
-								<td class="intro">
+								<td className="intro">
 									<h3>Sinicization of Christianity in China</h3>
 									<p align="justify">
 										Organized by the Fudan-European Centre for China Studies, UCPH and the Asia
@@ -48,10 +51,10 @@ class Books extends Component {
 								</td>
 							</tr>
 							<tr>
-								<td class="image">
-									<img src={require('../book_cover.png')} style={{ width: '70%' }} alt="" />
+								<td className="image">
+									<img src={require('../book_cover.png')} style={{ width: '90%' }} alt="" />
 								</td>
-								<td class="intro">
+								<td className="intro">
 									<p align="justify">
 										Commentary on China's reemergence as a world power tends to focus on the
 										country's unprecedented economic growth and implications for global security.
@@ -62,10 +65,10 @@ class Books extends Component {
 								</td>
 							</tr>
 							<tr>
-								<td class="image">
-									<img src={require('../20190314230626.png')} style={{ width: '70%' }} alt="" />
+								<td className="image">
+									<img src={require('../20190314230626.png')} style={{ width: '90%' }} alt="" />
 								</td>
-								<td class="intro">
+								<td className="intro">
 									<p align="justify">
 										近日有一个自称为“全能神教会”的团体，以耶稣基督教会为名，自2013
 										年1月起在香港、台湾各大报登载了超过十次的广告，2月起也开始在加拿大世界日报刊登全版广告，此举引起许多基督徒及福音朋友的反感与不安。
@@ -76,10 +79,10 @@ class Books extends Component {
 								</td>
 							</tr>
 							<tr>
-								<td class="image">
-									<img src={require('../20190314230636.png')} style={{ width: '70%' }} alt="" />
+								<td className="image">
+									<img src={require('../20190314230636.png')} style={{ width: '90%' }} alt="" />
 								</td>
-								<td class="intro">
+								<td className="intro">
 									<p align="justify">
 										80年代初期，中国改革开放，海外华人，成千上万，遍访祖国，其中基督徒为数不少，与国内信徒接触之间，不免有属灵交流。美国水流职事站和台湾福音书房出版之文字书刊，在海外早已流传甚广，深获基督教界各方人士欢迎，于该段时间内，也流传至国内各地。
 									</p>
@@ -89,10 +92,10 @@ class Books extends Component {
 								</td>
 							</tr>
 							<tr>
-								<td class="image">
-									<img src={require('../20190314230647.png')} style={{ width: '70%' }} alt="" />
+								<td className="image">
+									<img src={require('../20190314230647.png')} style={{ width: '90%' }} alt="" />
 								</td>
-								<td class="intro">
+								<td className="intro">
 									<p align="justify">
 										神爱世人，但除了犹太人是神的选民外，神似乎特别爱中国人。50年代起，华人神学家基督徒领袖便从中国大陆及亚洲分散到全世界各地，发扬光大主在马太福音二十八章十九节的伟大使命：“所以，你们要去，使万民作我的门徒，奉父、子、圣灵的名给他们施洗。”
 									</p>
@@ -102,10 +105,10 @@ class Books extends Component {
 								</td>
 							</tr>
 							<tr>
-								<td class="image">
-									<img src={require('../20190602005913.png')} style={{ width: '70%' }} alt="" />
+								<td className="image">
+									<img src={require('../20190602005913.png')} style={{ width: '90%' }} alt="" />
 								</td>
-								<td class="intro">
+								<td className="intro">
 									<p align="justify">
 										全能神教于2017 年10 月18 日在其网站上刊登了一份声明，完完全全否认他们参与绑架34
 										位中华福音团契（简称中福）的牧师一事，（中福称此事为“416
