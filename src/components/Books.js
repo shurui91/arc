c15763185553a5ccf261476f2e1343bda92a4364
Shurui Liu <shurui91@gmail.com>
2019-06-30 22:09:59 -0700

    books page updated, all books will be open in a new tab

--- src/components/Books.js
+++ src/components/Books.js
@@ -32,10 +32,14 @@ class Books extends Component {
 										主在上海的见证，起始于1926年，汪佩真姊妹看见主恢复的亮光，在她的住处新闸路赓庆里开始擘饼聚会。1927年，倪柝声弟兄和李渊如姊妹也先后参加赓庆里的聚会。同年3月，倪弟兄将福音书房从福州马尾的罗星塔迁往上海。1928年1月，上海基督徒聚会处迁入哈同路240弄文德里聚会。1927年至1942年这短短的15年时间里，上海成了倪弟兄职事工作的中心，他在这里出版属灵书报，召开友华村的训练。1942年，上海基督徒聚会处因误会将倪弟兄革除，之后有六年之久，聚会死沉、同工四散，没多久因日军侵略，聚会停止，会所关门。1948年，倪弟兄复职，回到了上海，带进上海教会的复兴。同年，上海教会买地盖建可容纳3000人的南阳路会所。1948年到1951年，是上海基督徒聚会处最蒙福的阶段，圣徒同心合意，兴旺福音。
 									</p>
 									<p>
-										<Button href={neeChinese}>阅读</Button>
+										<Button href={neeChinese} target="_blank">
+											阅读
+										</Button>
 									</p>
 									<p>
-										<Button href={neeEnglish}>Read</Button>
+										<Button href={neeEnglish} target="_blank">
+											Read
+										</Button>
 									</p>
 								</td>
 							</tr>
@@ -52,7 +56,9 @@ class Books extends Component {
 										在信仰方面，基督信仰自从唐朝传入中国，数百年内一直“水土不服”，不能融入中国社会。这与和合本圣经之前的各译本采用文言文或方言翻译有直接的关系。和合本圣经的翻译原则之一就是“字词应当是操官话的平民百姓所日常使用和明白的，书面用语和不大通用的用词应该避免；文句的结构应符合口语”。因此，二十世纪以来，和合本圣经一旦面世，销量很大，流传很广，基督信仰也就随着和合本圣经而入千家万户，影响社会各阶层，并且深入人心。圣经体现的价值观如“荣神益人”、“爱人如己”、“和平福音”、“披戴基督”等，对净化社会道德空气、维持邻里和睦、社会和谐，居功甚伟。
 									</p>
 									<p>
-										<Button href={hundredYears}>Read</Button>
+										<Button href={hundredYears} target="_blank">
+											阅读
+										</Button>
 									</p>
 								</td>
 							</tr>
@@ -68,7 +74,9 @@ class Books extends Component {
 										University
 									</p>
 									<p>
-										<Button href={combination}>Read</Button>
+										<Button href={combination} target="_blank">
+											Read
+										</Button>
 									</p>
 								</td>
 							</tr>
@@ -82,7 +90,9 @@ class Books extends Component {
 										country's unprecedented economic growth and implications for global security.
 									</p>
 									<p>
-										<Button href={kicus}>Read</Button>
+										<Button href={kicus} target="_blank">
+											Read
+										</Button>
 									</p>
 								</td>
 							</tr>
@@ -96,7 +106,9 @@ class Books extends Component {
 										年1月起在香港、台湾各大报登载了超过十次的广告，2月起也开始在加拿大世界日报刊登全版广告，此举引起许多基督徒及福音朋友的反感与不安。
 									</p>
 									<p>
-										<Button href={statement1}>Read</Button>
+										<Button href={statement1} target="_blank">
+											阅读
+										</Button>
 									</p>
 								</td>
 							</tr>
@@ -109,7 +121,9 @@ class Books extends Component {
 										80年代初期，中国改革开放，海外华人，成千上万，遍访祖国，其中基督徒为数不少，与国内信徒接触之间，不免有属灵交流。美国水流职事站和台湾福音书房出版之文字书刊，在海外早已流传甚广，深获基督教界各方人士欢迎，于该段时间内，也流传至国内各地。
 									</p>
 									<p>
-										<Button href={statement2}>Read</Button>
+										<Button href={statement2} target="_blank">
+											阅读
+										</Button>
 									</p>
 								</td>
 							</tr>
@@ -122,7 +136,9 @@ class Books extends Component {
 										神爱世人，但除了犹太人是神的选民外，神似乎特别爱中国人。50年代起，华人神学家基督徒领袖便从中国大陆及亚洲分散到全世界各地，发扬光大主在马太福音二十八章十九节的伟大使命：“所以，你们要去，使万民作我的门徒，奉父、子、圣灵的名给他们施洗。”
 									</p>
 									<p>
-										<Button href={statement3}>Read</Button>
+										<Button href={statement3} target="_blank">
+											阅读
+										</Button>
 									</p>
 								</td>
 							</tr>
@@ -140,26 +156,40 @@ class Books extends Component {
 										位被非法拘禁的中福牧师之一。第三部分含一份北京公安关于该事件的资料。被非法拘禁的人之中有一位姊妹最先逃了出来，中福的被害家属就决定将此事报到北京去，留了底。第四部分是我们根据申的书以及张的访谈写的一点对全能神教声明的反驳。
 									</p>
 									<p>
-										<Button href={rebuttalChinese}>阅读</Button>
+										<Button href={rebuttalChinese} target="_blank">
+											阅读
+										</Button>
 									</p>
 									<p>
-										<Button href={rebuttalEnglish}>Rebuttal</Button>
+										<Button href={rebuttalEnglish} target="_blank">
+											Rebuttal
+										</Button>
 									</p>
 									<p>
-										<Button href={elDenial}>Denial Statement</Button>
+										<Button href={elDenial} target="_blank">
+											Denial Statement
+										</Button>
 									</p>
 								</td>
 							</tr>
 							<tr>
 								<td className="image">
-									<img src={require('../events/Isaiah/Isaiah1.jpg')} style={{ width: '90%' }} alt="" />
+									<img
+										src={require('../events/Isaiah/Isaiah1.jpg')}
+										style={{ width: '90%' }}
+										alt=""
+									/>
 								</td>
 								<td className="intro">
 									<p style={{ textAlign: 'justify' }}>
-										《以赛亚书》最重要的希伯来文本是马所拉文本（Masoretic Text，简称MT）和死海古卷中的“大以赛亚书卷”（The Great Isaiah Scroll，简称1QIsaa）。本文逐字逐节比较这两种文本以定量确定其相似度：第一种比较方法是根据阿贝各、弗林特及吴睿智（Abegg，Flint，Ulrich）的分类，第二种方法则逐字比较字词的拼写。之后，本文指出大以赛亚书卷中对经文意义有重要价值的记载。本文基于国际学术界死海古卷最新的科研成果，对两种希伯来文本作第一手的定量分析，期盼对国内圣经学术界，特别是对汉语圣经考古学、圣经文本评断学、圣经汉译等研究，提出有益的参考。
+										《以赛亚书》最重要的希伯来文本是马所拉文本（Masoretic
+										Text，简称MT）和死海古卷中的“大以赛亚书卷”（The Great Isaiah
+										Scroll，简称1QIsaa）。本文逐字逐节比较这两种文本以定量确定其相似度：第一种比较方法是根据阿贝各、弗林特及吴睿智（Abegg，Flint，Ulrich）的分类，第二种方法则逐字比较字词的拼写。之后，本文指出大以赛亚书卷中对经文意义有重要价值的记载。本文基于国际学术界死海古卷最新的科研成果，对两种希伯来文本作第一手的定量分析，期盼对国内圣经学术界，特别是对汉语圣经考古学、圣经文本评断学、圣经汉译等研究，提出有益的参考。
 									</p>
 									<p>
-										<Button href={comparisonOfIsaiahScroll}>阅读</Button>
+										<Button href={comparisonOfIsaiahScroll} target="_blank">
+											阅读
+										</Button>
 									</p>
 								</td>
 							</tr>
