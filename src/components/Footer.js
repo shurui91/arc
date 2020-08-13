d3f525244022a4b8e5eaf42e564a47c8855cf17e
Shurui Liu <shurui91@gmail.com>
2020-08-13 15:47:28 -0700

    still working on the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,23 +1,87 @@
-import React, { Component } from 'react';
+import React, { Component, cloneElement } from 'react';
 import './Footer.css';
+import { Grid, Row, Col } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPhoneAlt } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
+import {
+	faFacebook,
+	faTwitter,
+	faInstagram,
+} from '@fortawesome/free-brands-svg-icons';
 
 class Footer extends Component {
 	render() {
 		return (
 			<div className='footer'>
 				<div className='footerContent'>
-					<p
-						style={{
-							color: 'white',
-							fontSize: '21px',
-							textAlign: 'justify',
-						}}>
-						"A center to promote a better understanding of Asia’s
-						rich cultures, languages and religions and how they
-						integrate into a diverse global environment.​"
-						​Information—to Enrich​-​Interaction—to
-						Exchange​-​Instruction—to Equip
-					</p>
+					<Row className='row'>
+						<Col class='column'>
+							<p class='contactTitle'>CONTACT US</p>
+							<ul class='footerList'>
+								<li>
+									<FontAwesomeIcon
+										icon={faPhoneAlt}
+										size='lg'
+									/>
+									<span>&nbsp;562-555-1234</span>
+								</li>
+								<li>
+									<FontAwesomeIcon
+										icon={faEnvelope}
+										size='lg'
+									/>
+									<span>
+										&nbsp;contactus@asiaresearchcenter.org
+									</span>
+								</li>
+								<li>
+									<FontAwesomeIcon
+										icon={faMapMarkerAlt}
+										size='lg'
+									/>
+									<span>
+										&nbsp;13615 Del Amo Blvd, Cerritos, CA
+										90703
+									</span>
+								</li>
+							</ul>
+						</Col>
+						<Col class='column'>
+							<p class='contactTitle'>STAY CONNECTED</p>
+							<p>
+								<span>
+									<FontAwesomeIcon
+										icon={faFacebook}
+										size='lg'
+									/>
+								</span>
+								<span>
+									&nbsp;&nbsp;&nbsp;&nbsp;
+									<FontAwesomeIcon
+										icon={faTwitter}
+										size='lg'
+									/>
+								</span>
+								<span>
+									&nbsp;&nbsp;&nbsp;&nbsp;
+									<FontAwesomeIcon
+										icon={faInstagram}
+										size='lg'
+									/>
+								</span>
+							</p>
+						</Col>
+						<Col class='column'>
+							<p class='contactTitle'>PARTNERS</p>
+							<img
+								style={{ width: '100%' }}
+								src={require('../assets/images/OCMDlogo2color.png')}
+								alt=''
+							/>
+						</Col>
+					</Row>
 				</div>
 			</div>
 		);
