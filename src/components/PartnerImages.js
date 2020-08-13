e1a8064e56dca73fe6ad1695dba90da472bf05ec
Shurui Liu <shurui91@gmail.com>
2020-08-12 20:06:33 -0700

    partner image width issue fixed

--- src/components/PartnerImages.js
+++ src/components/PartnerImages.js
@@ -5,17 +5,21 @@ import { Container, Row, Col } from 'react-bootstrap';
 class PartnerImages extends Component {
 	render() {
 		return (
-			<div className="partnersImage">
-				<Row>
-					<Col sm={12}>
-						<a href="https://ocmusicdance.org/">
-							<img
-								style={{ width: '100%' }}
-								src={require('../20190601193138.jpg')} alt=""
-							/>
-						</a>
-					</Col>
-				</Row>
+			<div>
+				{/* fluid will make its width to be 100% */}
+				<Container fluid>
+					<Row>
+						<Col sm={12}>
+							<a href='https://ocmusicdance.org/'>
+								<img
+									style={{ width: '100%' }}
+									src={require('../20190601193138.jpg')}
+									alt=''
+								/>
+							</a>
+						</Col>
+					</Row>
+				</Container>
 			</div>
 		);
 	}
