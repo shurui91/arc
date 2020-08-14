92d79a97560e54e83cf92d37ad22c3579a4cf478
Shurui Liu <shurui91@gmail.com>
2020-08-14 01:02:20 -0700

    modify order of current events

--- src/components/Events.js
+++ src/components/Events.js
@@ -66,6 +66,60 @@ class Events extends Component {
 							<tr>
 								<td class='image'>
 									<img
+										src={require('../assets/images/20200205225445.jpg')}
+										style={{ width: '90%' }}
+										alt=''
+									/>
+								</td>
+								<td class='intro'>
+									<h3>
+										2017国际学术研讨会
+										邪教的观念暨全能神教之个案研究
+									</h3>
+									<p>September 15 - 16, 2017</p>
+								</td>
+							</tr>
+							<tr>
+								<td class='image'>
+									<img
+										src={require('../assets/images/20200205225359.jpg')}
+										style={{ width: '90%' }}
+										alt=''
+									/>
+								</td>
+								<td class='intro'>
+									<h3>
+										2015《圣经》与文学阐释国际学术研讨会
+									</h3>
+									<Link to='/2015bible'>
+										<Button
+											type='button'
+											className='float-middle'>
+											More Pictures
+										</Button>
+									</Link>
+								</td>
+							</tr>
+							<tr>
+								<td class='image'>
+									<img
+										src={require('../assets/images/20200205225143.jpg')}
+										style={{ width: '90%' }}
+										alt=''
+									/>
+								</td>
+								<td class='intro'>
+									<h3>
+										Translation of Bible and Comparative
+										Scripture
+									</h3>
+									<p>October 24, 2014</p>
+									<p>Beijing Minzu University of China</p>
+								</td>
+							</tr>
+							<tr>
+								<td class='image'>
+									<img
 										src={require('../events/deadsea/deadseascrolls2.jpg')}
 										style={{ width: '90%' }}
 										alt=''
@@ -119,60 +173,6 @@ class Events extends Component {
 											More Pictures
 										</Button>
 									</Link>
-								</td>
-							</tr>
-							<tr>
-								<td class='image'>
-									<img
-										src={require('../assets/images/20200205225445.jpg')}
-										style={{ width: '90%' }}
-										alt=''
-									/>
-								</td>
-								<td class='intro'>
-									<h3>
-										2017国际学术研讨会
-										邪教的观念暨全能神教之个案研究
-									</h3>
-									<p>September 15 - 16, 2017</p>
-								</td>
-							</tr>
-							<tr>
-								<td class='image'>
-									<img
-										src={require('../assets/images/20200205225359.jpg')}
-										style={{ width: '90%' }}
-										alt=''
-									/>
-								</td>
-								<td class='intro'>
-									<h3>
-										2015《圣经》与文学阐释国际学术研讨会
-									</h3>
-									<Link to='/2015bible'>
-										<Button
-											type='button'
-											className='float-middle'>
-											More Pictures
-										</Button>
-									</Link>
-								</td>
-							</tr>
-							<tr>
-								<td class='image'>
-									<img
-										src={require('../assets/images/20200205225143.jpg')}
-										style={{ width: '90%' }}
-										alt=''
-									/>
-								</td>
-								<td class='intro'>
-									<h3>
-										Translation of Bible and Comparative
-										Scripture
-									</h3>
-									<p>October 24, 2014</p>
-									<p>Beijing Minzu University of China</p>
 								</td>
 							</tr>
 							<tr>
