d5b725077f15a18356a557c26cd33e31a945d79d
Shurui Liu <shurui91@gmail.com>
2020-08-14 21:14:18 -0700

    more quick links added to homepage

--- src/components/UpdateList.js
+++ src/components/UpdateList.js
@@ -4,6 +4,8 @@ import { Card, Container, Row, Col, ListGroup } from 'react-bootstrap';
 import neeChinese from '../books/The Nee Yu-Cheng Family_Chinese.pdf';
 import CUVBible from '../books/The CUV Bible and Bible Translations in Chinese.pdf';
 import hundredYears from '../books/hunderdYears.pdf';
+import EasternFlash from '../books/2018EasternFlash.pdf';
+import combination from '../books/combination.pdf';
 
 class UpdateList extends Component {
 	render() {
@@ -44,6 +46,19 @@ class UpdateList extends Component {
 											style={{ textAlign: 'left' }}>
 											和合本圣经百周年纪念
 										</ListGroup.Item>
+										<ListGroup.Item
+											action
+											href={EasternFlash}
+											style={{ textAlign: 'left' }}>
+											2018东方闪电（全能神）应对策略研讨会
+										</ListGroup.Item>
+										<ListGroup.Item
+											action
+											href={combination}
+											style={{ textAlign: 'left' }}>
+											Sinicization of Christianity in
+											China
+										</ListGroup.Item>
 									</ListGroup>
 								</Card.Text>
 							</Card>
@@ -73,6 +88,17 @@ class UpdateList extends Component {
 											style={{ textAlign: 'left' }}>
 											2017国际学术研讨会
 											邪教的观念暨全能神教之个案研究
+										</ListGroup.Item>
+										<ListGroup.Item
+											action
+											href='/2015bible'
+											style={{ textAlign: 'left' }}>
+											2015《圣经》与文学阐释国际学术研讨会
+										</ListGroup.Item>
+										<ListGroup.Item
+											style={{ textAlign: 'left' }}>
+											Translation of Bible and Comparative
+											Scripture
 										</ListGroup.Item>
 									</ListGroup>
 								</Card.Text>
