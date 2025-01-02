c5397f246d1136fc38ba21d3157db6d0c3999f51
Aaron Liu <shurui91@gmail.com>
2025-01-02 10:44:42 -0800

    copyright component is updated

--- src/components/Copyright.js
+++ src/components/Copyright.js
@@ -1,19 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Copyright.css';
 
-class Copyright extends Component {
-	constructor(props) {
-		super(props);
-		this.state = { year: new Date().getFullYear() };
-	}
+const Copyright = () => {
+	const currentYear = new Date().getFullYear();
 
-	render() {
-		return (
-			<div className='copyright'>
-				<p>&copy; {this.state.year} ARC. All Rights Reserved</p>
-			</div>
-		);
-	}
-}
+	return (
+		<div className="copyright">
+			<p>&copy; {currentYear} ARC. All Rights Reserved</p>
+		</div>
+	);
+};
 
 export default Copyright;
