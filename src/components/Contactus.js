dd411604127cb3e678a87af443a086250395e629
Aaron Liu <shurui91@gmail.com>
2025-01-02 10:55:44 -0800

    contact us component is updated

--- src/components/Contactus.js
+++ src/components/Contactus.js
@@ -1,63 +1,21 @@
-import React, { Component } from 'react';
-import { Form, Button } from "react-bootstrap";
+import React from 'react';
 import './Contactus.css';
-import Axios from 'axios';
 
-
-class Contactus extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			fname: '',
-			lname: '',
-			email: '',
-			message: '',
-			mailSent: false,
-			error: null
-		}
-	}
-
-	handleFormSubmit(event) {
-		event.preventDefault();
-		console.log(this.state);
-	}
-
-	render() {
-		return (
-			<div className="App">
-				<p>Contact Me</p>
-				<div>
-					<form action="#">
-						<label>First Name</label>
-						<input type="text" id="fname" name="firstname" placeholder="Your name.."
-							value={this.state.fname}
-							onChange={e => this.setState({ fname: e.target.value })}
-						/>
-						<label>Last Name</label>
-						<input type="text" id="lname" name="lastname" placeholder="Your last name.."
-							value={this.state.lname}
-							onChange={e => this.setState({ lname: e.target.value })}
-						/>
-
-
-						<label>Email</label>
-						<input type="email" id="email" name="email" placeholder="Your email"
-							value={this.state.email}
-							onChange={e => this.setState({ email: e.target.value })}
-						/>
-
-
-						<label>Message</label>
-						<textarea id="message" name="message" placeholder="Write something.."
-							onChange={e => this.setState({ message: e.target.value })}
-							value={this.state.message}
-						></textarea>
-						<input type="submit" onClick={e => this.handleFormSubmit(e)} value="Submit" />
-					</form>
-				</div>
-			</div >
-		);
-	}
-}
+const Contactus = () => {
+    return (
+        <div className='contactus-container'>
+            <h2 className='contactus-title'>Contact Us</h2>
+            <p className='contactus-text'>
+                For any inquiries, please send us an email at:
+                <br />
+                <a
+                    href='mailto:contactus@asiaresearchcenter.org'
+                    className='contactus-email'>
+                    contactus@asiaresearchcenter.org
+                </a>
+            </p>
+        </div>
+    );
+};
 
 export default Contactus;
