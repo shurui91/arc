d47fb9b247ba4be39b6ffb1055ae3c260d600024
Aaron Liu <shurui91@gmail.com>
2025-01-09 09:33:29 -0800

    Support page is updated

---
+++ src/components/Support.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import './Support.css';
+import Copyright from './Copyright';
+import { Container, Jumbotron } from 'react-bootstrap';
+
+const Support = () => {
+    return (
+        <>
+            <div className='support'>
+                <Jumbotron style={{ backgroundColor: 'pink', padding: '2rem' }}>
+                    <Container>
+                        <h1>Support ARC</h1>
+                        <p>
+                            To support ARC, please send your donation via money
+                            order or check to:
+                        </p>
+                        <address>
+                            Asia Research Center
+                            <br />
+                            27611 La Paz Road
+                            <br />
+                            Laguna Niguel, CA 92677
+                        </address>
+                        <p>
+                            For more information, please email us to{' '}
+                            <a href='mailto:abrahamtho@gmail.com'>
+                                abrahamtho@gmail.com
+                            </a>
+                        </p>
+                    </Container>
+                </Jumbotron>
+            </div>
+            <Copyright />
+        </>
+    );
+};
+
+export default Support;
