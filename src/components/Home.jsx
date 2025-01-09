3e67bd30e82312cd705de498c8d2b7b8610aec0d
Aaron Liu <shurui91@gmail.com>
2025-01-09 09:37:41 -0800

    Home component renamed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,34 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Home.css';
-import soundfile from '../assets/Sign of the Times.mp3';
-// import Sound from 'react-sound';
 import Carousel from './Carousel';
 import Vision from './Vision';
 import UpdateList from './UpdateList';
 import PartnerImages from './PartnerImages';
-import Copyright from './Copyright';
 import Footer from './Footer';
+import Copyright from './Copyright';
 
-class Home extends Component {
-	render() {
-		return (
-			<div>
-				<Carousel />
-				<UpdateList />
-				<Vision />
-				<PartnerImages />
-				<Footer />
-				<Copyright />
-				{/* <Sound
-					url={soundfile}
-					playStatus={Sound.status.PLAYING}
-					onLoading={this.handleSongLoading}
-					onPlaying={this.handleSongPlaying}
-					onFinishedPlaying={this.handleSongFinishedPlaying}
-				/> */}
-			</div>
-		);
-	}
-}
+const Home = () => {
+    return (
+        <>
+            <div className='home'>
+                <Carousel />
+                <UpdateList />
+                <Vision />
+                <PartnerImages />
+                <Footer />
+                {/* <audio src={soundfile} autoPlay loop>
+        Your browser does not support the audio element.
+      </audio> */}
+            </div>
+            <Copyright />
+        </>
+    );
+};
 
 export default Home;
