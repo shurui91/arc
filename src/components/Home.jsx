9a7013f3926abf38398cb6329ead9a810412ec3f
Aaron Liu <shurui91@gmail.com>
2025-01-22 08:52:36 -0800

    updateList is hidden

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
         <>
             <div className='home'>
                 <Carousel />
-                <UpdateList />
+                {/* <UpdateList /> */}
                 {/* <Vision /> */}
                 <PartnerImages />
                 <Footer />
